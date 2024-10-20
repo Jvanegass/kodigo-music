bfd993df5a46ffc735c559cb3371ed0cbba79532
JuanMauricioMV <jvanegass2019@gmail.com>
2024-10-20 02:11:14 -0600

    NUEVO CAMBIO

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,10 @@ import MainContent from '../components/maincontent';
 const Home = () => {
   return (
     <div className="flex h-screen">
+      {/* Sidebar Fijo */}
       <Sidebar />
+      
+      {/* Contenido Principal */}
       <div className="flex-1 flex flex-col">
         <Header />
         <MainContent />
